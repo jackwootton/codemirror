f1412672b9c5618e9f0c97b95caa82bb8c42ca10
Jack Wootton <jackwootton@gmail.com>
2021-06-12 16:41:04 +0000

    Code rendering

--- cm/src/app/components/cm/cm.component.ts
+++ cm/src/app/components/cm/cm.component.ts
@@ -1,23 +1,41 @@
-import { AfterViewInit, Component, ElementRef, Input, Optional, Self, ViewChild } from '@angular/core';
+import { FocusMonitor } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, ElementRef, forwardRef, HostBinding, Input, OnDestroy, Optional, Self, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
 import { basicSetup, EditorState } from "@codemirror/basic-setup";
 import { json } from "@codemirror/lang-json";
 import { EditorView } from "@codemirror/view";
-import { Observable } from 'rxjs';
-import { Subject } from 'rxjs/internal/Subject';
+import { Subject } from 'rxjs';
 
 
 @Component({
   selector: 'app-cm',
   templateUrl: './cm.component.html',
-  styleUrls: ['./cm.component.scss']
+  styleUrls: ['./cm.component.scss'],
+  providers: [
+    {
+      provide: MatFormFieldControl,
+      useExisting: forwardRef(() => CmComponent),
+      multi: true,
+    },
+  ],
 })
 export class CmComponent implements MatFormFieldControl<string>,
-  ControlValueAccessor, AfterViewInit {
-  @ViewChild('host', { static: false }) host?: ElementRef;
+  ControlValueAccessor, AfterViewInit, OnDestroy {
 
-  constructor(@Optional() @Self() public ngControl: NgControl) {
+  static nextId = 0;
+
+  @ViewChild('host', { static: false }) host?: ElementRef;
+  cm: EditorView = new EditorView();
+  nativeElement: any;
+
+  constructor(
+    @Optional() @Self() public ngControl: NgControl,
+    private readonly fm: FocusMonitor,
+    private readonly er: ElementRef,
+  ) {
+    this.nativeElement = er.nativeElement;
+
     // The NgControl for this control.
     if (this.ngControl) {
       // Setting the value accessor directly (instead of using
@@ -26,54 +44,64 @@ export class CmComponent implements MatFormFieldControl<string>,
     }
   }
 
-  // component lifecycle hooks
+
+  // Component lifecycle hooks.
   ngAfterViewInit(): void {
     let darkTheme = EditorView.theme({}, { dark: false });
+    this.cm = new EditorView({
+      parent: this.er.nativeElement,
+      state: EditorState.create(
+        {
+          doc: this._value || "",
+          extensions: [basicSetup, json(), darkTheme],
+        }),
+    });
+  }
 
-    if (this.host) {
-      let view = new EditorView({
-        parent: this.host.nativeElement,
-        state: EditorState.create(
-          {
-            extensions: [basicSetup, json(), darkTheme],
-          }),
-      });
-
-      console.log(view);
-    }
+  ngOnDestroy() {
+    this.stateChanges.complete();
   }
 
   // implement ControlValueAccessor
-  
+
   writeValue(obj: any): void {
-    throw new Error('Method not implemented.');
+    this.value = obj;
   }
+
   registerOnChange(fn: any): void {
-    throw new Error('Method not implemented.');
+    // Store the provided function as an internal method.
+    // this._onChange = fn;
   }
+
   registerOnTouched(fn: any): void {
-    throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');
   }
 
   // implement MatFormFieldControl<string>
 
   @Input()
   get value(): string | null {
-    return this._value;
+    return this.cm.state.doc.toString();
   }
   set value(value: string | null) {
+    // create the transaction
+    const transaction = this.cm.state.update({
+      changes: {
+        from: 0,
+        to: this.cm.state.doc.length,
+        insert: value || "",
+      },
+    });
 
-    EditorState.create(
-      {
-        doc: value || "",
-      }
-    );
+    this.cm.dispatch(transaction);
+    this._value = value;
+    this.stateChanges.next();
   }
   _value: string | null = "";
 
 
-  stateChanges: Observable<void> = new Subject<void>();
-  id: string = "";
+  stateChanges: Subject<void> = new Subject<void>();
+  id: string = `codemirror-${CmComponent.nextId++}`;
   placeholder: string = "";
   // ngControl: NgControl | null;
   focused: boolean = false;
@@ -86,8 +114,11 @@ export class CmComponent implements MatFormFieldControl<string>,
   autofilled?: boolean | undefined;
   userAriaDescribedBy?: string | undefined;
 
-  setDescribedByIds(ids: string[]): void {
-    throw new Error('Method not implemented.');
+  // Used by the <mat-form-field> to specify the IDs that should be used for
+  // the aria-describedby attribute.
+  @HostBinding('attr.aria-describedby') describedBy = '';
+  setDescribedByIds(ids: string[]) {
+    this.describedBy = ids.join(' ');
   }
 
   onContainerClick(event: MouseEvent): void {
